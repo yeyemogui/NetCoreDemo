b19019f1226f3e1132e1a9aa88d2767881fc9563
sleeping <sleep.flyer@hotmail.com>
2017-12-23 21:20:18 +0800

    first commit

---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength =3)]
+        [Required]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        [Range(typeof(DateTime), "1/1/1966","1/1/2020")]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        public string Genre { get; set; }
+        [Range(0, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+}
